9711e2215ffa332f1047f3f7b71f1e73d0f900e5
Abdelrahman fayez <abdelrahmannfayez@gmail.com>
2024-03-09 22:16:26 +0200

    Vigenère Auto & Repeating

--- securitylibrary/MainAlgorithms/AutokeyVigenere.cs
+++ securitylibrary/MainAlgorithms/AutokeyVigenere.cs
@@ -11,22 +11,77 @@ namespace SecurityLibrary
         public string Analyse(string plainText, string cipherText)
         {
 
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            plainText = plainText.ToLower();
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            string NKey = "";
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                int idx = alpha.IndexOf(cipherText[i]) - alpha.IndexOf(plainText[i]);
+                if (idx < 0) idx += 26;
+                idx = idx % 26;
+                NKey += alpha[idx];
+            }
+            string temp = "";
+            // Brute force over all combinations.
+            for (int i = 0; i < NKey.Length; i++)
+            {
+                temp += NKey[i];
+                //Console.WriteLine(temp);
+
+                if (plainText.Equals(Decrypt(cipherText, temp).ToLower()))
+                {
+
+                    return temp;
+                }
+            }
+            return NKey;
         }
 
         public string Decrypt(string cipherText, string key)
         {
+            cipherText = cipherText.ToLower();
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            string Nkey = key;
+            int add = 0;
+           
+            string plain = "";
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                int idx = alpha.IndexOf(cipherText[i]) - alpha.IndexOf(Nkey[i]);
+                if (idx < 0) idx += 26;
+                idx = idx % 26;
+                plain += alpha[idx];
+                Nkey += plain[i];
+            }
 
-            throw new NotImplementedException();
+            return plain.ToLower();
 
         }
 
         public string Encrypt(string plainText, string key)
         {
-            
-             throw new NotImplementedException();
-      
-           
-    }
+
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            string Nkey = key;
+            int add = 0;
+            while (Nkey.Length < plainText.Length)
+            {
+                if (add == plainText.Length)
+                    add = 0;
+                Nkey += plainText[add];
+                add++;
+            }
+            string cipher = "";
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                int idx = alpha.IndexOf(plainText[i]) + alpha.IndexOf(Nkey[i]);
+                idx = idx % 26;
+                cipher += alpha[idx];
+            }
+            return cipher;
+
+
+        }
     }
 }
