150e063ab903b58faf261b2c90fd2b562b3a904c
Ahmed hany <hamm0o0dhany@gmail.com>
2024-03-28 13:50:12 +0200

    ExtendedEuclid impelementation

--- securitylibrary/MainAlgorithms/ExtendedEuclid.cs
+++ securitylibrary/MainAlgorithms/ExtendedEuclid.cs
@@ -16,7 +16,45 @@ namespace SecurityLibrary.AES
         /// <returns>Mul inverse, -1 if no inv</returns>
         public int GetMultiplicativeInverse(int number, int baseN)
         {
-            throw new NotImplementedException();
+            if (AreRelativelyPrime(number, baseN))
+            {
+                long number1 = (long)number;
+                int  i = 1;
+                while (true)
+                {
+                    if ((number1 * i) % baseN == 1)
+                        return i;
+                    else
+                        i++;
+                }
+            }
+            else { return -1; }
         }
+
+        public bool AreRelativelyPrime(int a, int b)
+        {
+            if (a<b)
+            for (int i = 2; i <= a; i++)
+            {
+                // If both numbers are divisible by i, they are not relatively prime
+                if (a % i == 0 && b % i == 0)
+                {
+                    return false;
+                }
+            }
+            else
+                for (int i = 2; i <=  b; i++)
+                {
+                    // If both numbers are divisible by i, they are not relatively prime
+                    if (a % i == 0 && b % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+            // If no common divisor found, they are relatively prime
+            return true;
+        }
+
     }
 }
