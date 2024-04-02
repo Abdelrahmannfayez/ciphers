7f22f12c7a83c0f9c9541b3e58a9f659b22f1899
Nour Ashraf <nourelhudaashraf20@gmail.com>
2024-04-02 12:53:33 +0200

    add shiftRows and InvertShiftRows takes string and return shifted string

--- securitylibrary/AES/AES.cs
+++ securitylibrary/AES/AES.cs
@@ -20,5 +20,35 @@ namespace SecurityLibrary.AES
         {
             throw new NotImplementedException();
         }
+        private string shiftRows(string plainText)
+        {
+            return shift(plainText, false, 2);
+        }
+
+        private string InvShiftRows(string shiftedText)
+        {
+            return shift(shiftedText, true, 6);
+        }
+
+        private string shift(string text, bool invert, int index)
+        {
+            string output = text.Substring(0, 10);
+            int startIndex = 10;
+            int endIndex = 18;
+            while (endIndex <= text.Length + 1)
+            {
+                int countEnd = endIndex - (startIndex + index);
+                output += text.Substring(startIndex + index, countEnd);
+                int countShiftedChars = (startIndex + index) - startIndex;
+                output += text.Substring(startIndex, countShiftedChars);
+                startIndex = endIndex;
+                endIndex += 8;
+                if (invert)
+                    index -= 2;
+                else
+                    index += 2;
+            }
+            return output;
+        }
     }
 }
