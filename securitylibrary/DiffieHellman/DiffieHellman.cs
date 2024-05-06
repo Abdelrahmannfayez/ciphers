eee747d66122ace8453b501eede987108854a146
AhmedEsmat01 <essmat884@gmail.com>
2024-05-07 02:02:15 +0300

    finished MS5 , all tests pass

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -14,13 +14,28 @@ namespace SecurityLibrary.DiffieHellman
 
       public  int power(int f, int s, int sf)
         {
-            throw new NotImplementedException();
+            if (s == 0)
+                return 1;
+            else if (s % 2 == 0)
+            {
+                int half = power(f, s / 2, sf);
+                return (half * half) % sf;
+            }
+            else
+            {
+                return (f * power(f, s - 1, sf)) % sf;
+            }
         }
 
-       public List<int> GetKeys(int q, int alpha, int xa, int xb)
+        public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
+            int ya = power(alpha, xa, q);
+            int yb = power(alpha, xb, q);
 
-            throw new NotImplementedException();
+            int ka = power(yb, xa, q);
+            int kb = power(ya, xb, q);
+
+            return new List<int> { ka, kb };
         }
     }
 }
--- securitylibrary/ElGamal/ELGAMAL.cs
+++ securitylibrary/ElGamal/ELGAMAL.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SecurityLibrary.ElGamal
 {
@@ -11,25 +8,86 @@ namespace SecurityLibrary.ElGamal
         /// <summary>
         /// Encryption
         /// </summary>
-        /// <param name="alpha"></param>
         /// <param name="q"></param>
+        /// <param name="alpha"></param>
         /// <param name="y"></param>
         /// <param name="k"></param>
+        /// <param name="m"></param>
         /// <returns>list[0] = C1, List[1] = C2</returns>
-        /// 
-
         public List<long> Encrypt(int q, int alpha, int y, int k, int m)
         {
+            List<long> cipher = new List<long>();
 
-            throw new NotImplementedException();
+            long c1 = ModPower(alpha, k, q);
 
+            long c2 = (m * ModPower(y, k, q)) % q;
+
+            cipher.Add(c1);
+            cipher.Add(c2);
+
+            return cipher;
         }
-     
+
+        /// <summary>
+        /// Decryption
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <param name="x"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
         public int Decrypt(int c1, int c2, int x, int q)
         {
+            long S = ModPower(c1, x, q);
 
-            throw new NotImplementedException();
+            long m = (c2 * ModInverse(S, q)) % q;
 
+            return (int)m;
+        }
+
+        private long ModPower(long baseNum, long exponent, long modulus)
+        {
+            if (modulus == 1) return 0;
+
+            long result = 1;
+            baseNum = baseNum % modulus;
+
+            while (exponent > 0)
+            {
+                if (exponent % 2 == 1)
+                    result = (result * baseNum) % modulus;
+                exponent = exponent >> 1;
+                baseNum = (baseNum * baseNum) % modulus;
+            }
+
+            return result;
+        }
+
+        private long ModInverse(long a, long m)
+        {
+            long m0 = m;
+            long y = 0, x = 1;
+
+            if (m == 1)
+                return 0;
+
+            while (a > 1)
+            {
+                long q = a / m;
+                long t = m;
+
+                m = a % m;
+                a = t;
+                t = y;
+
+                y = x - q * y;
+                x = t;
+            }
+
+            if (x < 0)
+                x += m0;
+
+            return x;
         }
     }
 }
